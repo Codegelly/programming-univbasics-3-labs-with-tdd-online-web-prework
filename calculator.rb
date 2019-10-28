035d2d861c6d6afa419605d510c0f66a6db48c5b
Angelica  Thompson <i.thompson89@outlook.com>
2019-10-28 22:58:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 first_number = 10
 second_number = 13 
 #23 = sum
@@ -10,3 +11,12 @@ sum = first_number + second_number
 difference = first_number - second_number
 product = second_number * first_number
 quotient = first_number/ second_number
+=======
+10 
+13
+expect(13).
+not_to equel(0)
+10+13 = 23
+13-10 = 3
+13/10 = 1 
+>>>>>>> 1341f2a801225b0319d3b27176068914c2558599
